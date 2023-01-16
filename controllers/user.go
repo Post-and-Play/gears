bfcf258ff460b894b00b767749e0ce7f00d41d80
Johnman67112 <joaovitorcucomo@gmail.com>
2023-01-16 20:03:57 -0300

    base user crud

--- controllers/user.go
+++ controllers/user.go
@@ -1,1 +1,75 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/Post-and-Play/gears/infra"
+	"github.com/Post-and-Play/gears/models"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	if err := models.UserValidator(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	infra.DB.Create(&user)
+
+	c.JSON(http.StatusOK, user)
+}
+
+func GetUser(c *gin.Context) {
+	var user models.User
+	id := c.Params.ByName("id")
+
+	infra.DB.First(&user, id)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "User not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func EditUser(c *gin.Context) {
+	var user models.User
+	id := c.Params.ByName("id")
+
+	infra.DB.First(&user, id)
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	if err := models.UserValidator(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	infra.DB.Model(&user).UpdateColumns(user)
+	c.JSON(http.StatusOK, user)
+}
+
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	id := c.Params.ByName("id")
+
+	infra.DB.Delete(&user, id)
+
+	c.JSON(http.StatusOK, gin.H{"data": "User deleted sucessfully"})
+}
