b5fd7f25cc94daafd4d0ead96c417e791032f3c4
Johnman67112 <joaovitorcucomo@gmail.com>
2023-06-01 23:58:06 -0300

    swag, version, edit user and routes

--- models/user.go
+++ models/user.go
@@ -30,3 +30,27 @@ func UserValidator(user *User) error {
 
 	return nil
 }
+
+type EditUser struct {
+	ID          uint   `json:"id" validate:"nonzero"`
+	Name        string `json:"name"`
+	UserName    string `json:"user_name"`
+	Password    string `json:"password"`
+	Mail        string `json:"mail"`
+	Description string `json:"description"`
+	PhotoAdr    string `json:"photo_adr"`
+	TopAdr      string `json:"top_adr"`
+	EpicUser    string `json:"epic_user"`
+	SteamUser   string `json:"steam_user"`
+	DiscordUser string `json:"discord_user"`
+	GithubUser  string `json:"github_user"`
+	TwitchUser  string `json:"twitch_user"`
+}
+
+func EditUserValidator(user *EditUser) error {
+	if err := validator.Validate(user); err != nil {
+		return err
+	}
+
+	return nil
+}
