c6394da87f55fd063e426899749a8e13d1e977a4
Rafael Santana <rafaelsantana.projetos@gmail.com>
2023-10-03 16:46:45 -0300

    Fix PATCH Method not enable

--- ui/middlewares.go
+++ ui/middlewares.go
@@ -6,7 +6,7 @@ import (
 
 func AddDefaultHandlers(c *gin.Context) {
 	c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Add("Access-Control-Allow-Methods", "GET,PUT,OPTIONS,POST,DELETE")
+	c.Writer.Header().Add("Access-Control-Allow-Methods", "GET,PUT,OPTIONS,POST,DELETE,PATCH")
 	c.Writer.Header().Add("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token")
 
 	if c.Request.Method == "OPTIONS" {
